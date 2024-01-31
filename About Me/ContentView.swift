79a26aa0de26b5e768c9c0944d3ab82ab4b05ab8
RawanOs <89946063+RawanOs@users.noreply.github.com>
2024-01-31 21:54:49 +0300

    add about me view

--- About Me/ContentView.swift
+++ About Me/ContentView.swift
@@ -9,15 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 5) {
+            AboutmeView
+            Spacer()
+            ContactView
         }
         .padding()
     }
 }
+
+extension ContentView {
+    var AboutmeView: some View {
+        VStack(spacing: 5) {
+            Image("Image")
+                .clipShape(Circle())
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Rawan Al-sufyani")
+                .bold()
+                .font(.system(size: 32))
+            Text("junior IOS developer at Elm Company 👩🏻‍💻")
+                .bold()
+                .font(.system(size: 17))
+            Text("Bachelor of Computer science with First Honors 🥇")
+                .font(.system(size: 17))
+            Text("The magic, in my opinion, is the creative collaboration of design and technology ✨")
+                .font(.system(size: 17))
+        }
+    }}
+
+extension ContentView {
+    var ContactView: some View {
+        VStack(spacing: 7){
+            HStack(spacing: 5){
+                Image(systemName: "phone.circle")
+                Text("0506314345")
+            }
+            HStack(spacing: 5){
+                Image(systemName: "envelope.circle")
+                Text("RawanOmar1998@hotmail.com")
+                    .font(.system(size: 15))
+                    
+            }.padding(.leading)
+            
+          }.padding()
+        }
+    }
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
